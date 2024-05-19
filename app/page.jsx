1e438467f3574868571322405dad95f960d8ea61
sy-eggplant <sy.eggplant@gmail.com>
2024-05-19 19:23:46 +0900

    不要な要素を削除

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,11 @@
 import React from 'react';
 
 import Hero from '../components/Hero';
-import Content from '../components/Content';
 
 export default function Index() {
   return (
     <>
       <Hero />
-      <hr />
-      <Content />
     </>
   );
 }
